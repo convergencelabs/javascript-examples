70acfb9a07792314526bbd2850ae37b34aefcc00
Michael MacFadden <michael@convergencelabs.com>
2017-04-16 21:13:48 -0500

    Fixed the TODO example.

--- src/todo/model.js
+++ src/todo/model.js
@@ -8,17 +8,19 @@
    * @param {object} storage A reference to the client side storage class
    */
   function Model(name) {
-    this.loaded = Convergence.connectAnonymously(DOMAIN_URL).then(function (domain) {
+    this.loaded = Convergence.connectAnonymously(DOMAIN_URL).then(domain => {
       this.domain = domain;
-      return domain.models().open('examples', name, function (collectionId, modelId) {
-        return {
+      return domain.models().openAutoCreate({
+        collection: 'examples',
+        id: name,
+        data: {
           todos: []
-        };
-      });
-    }.bind(this)).then(function (model) {
+        }
+      })
+    }).then(model => {
       this.rtModel = model;
       this.rtTodos = model.elementAt('todos');
-    }.bind(this));
+    });
   }
 
   Model.prototype = {
@@ -26,7 +28,7 @@
     getViewTodos: function () {
       var viewTodos = [];
       this.rtTodos.forEach(function (rtTodo) {
-        if(!this.filter || this.filter === 'all') {
+        if (!this.filter || this.filter === 'all') {
           viewTodos.push(rtTodo.value());
         } else if (this.filter === 'active' && !rtTodo.value().completed) {
           viewTodos.push(rtTodo.value());
@@ -51,7 +53,7 @@
     setFilter: function (filter) {
       this.filter = filter;
     },
-    getTodo: function(id) {
+    getTodo: function (id) {
       var todo;
       this.rtTodos.forEach(function (rtTodo) {
         if (!todo && rtTodo.get('id').value() === id) {
@@ -60,7 +62,7 @@
       });
       return todo;
     },
-    getTodoIndex: function(id) {
+    getTodoIndex: function (id) {
       var index = -1;
       this.rtTodos.forEach(function (rtTodo, i) {
         if (index < 0 && rtTodo.get('id').value() === id) {
@@ -75,18 +77,18 @@
         rtTodo.get('completed').value(!rtTodo.get('completed').value());
       }
     },
-    toggleAll: function(completed) {
+    toggleAll: function (completed) {
       this.rtTodos.forEach(function (rtTodo) {
         rtTodo.set('completed', completed);
       });
     },
-    addTodo: function(todo) {
+    addTodo: function (todo) {
       this.rtTodos.push(todo);
     },
-    editTodo: function(id, data) {
+    editTodo: function (id, data) {
       var rtTodo = this.getTodo(id);
       if (rtTodo) {
-        for(var key in data) {
+        for (var key in data) {
           // the syntax here is important.  We could just call rtTodo.set(key, dta[key]), but that would
           // blow away any listeners on that rtString (the value of the "title" property), because we're
           // replacing the value via the parent.
@@ -95,53 +97,53 @@
         }
       }
     },
-    moveTodo: function(id, idToReplace) {
+    moveTodo: function (id, idToReplace) {
       var index = this.getTodoIndex(id);
       var newIndex = this.getTodoIndex(idToReplace);
       if (newIndex >= 0) {
         this.rtTodos.reorder(index, newIndex);
       }
     },
-    deleteTodo: function(id) {
+    deleteTodo: function (id) {
       var index = this.getTodoIndex(id);
       if (index >= 0) {
         this.rtTodos.remove(index);
       }
     },
-    deleteCompletedTodos: function() {
+    deleteCompletedTodos: function () {
       // start at the end so the indices don't shift while we're deleting elements
-      for(var i = this.rtTodos.length(); i--; i >= 0) {
+      for (var i = this.rtTodos.length(); i--; i >= 0) {
         if (this.rtTodos.get(i).get('completed').value() === true) {
           this.rtTodos.remove(i);
         }
       }
     },
-    onRemote: function(eventType, callback) {
-      switch(eventType) {
+    onRemote: function (eventType, callback) {
+      switch (eventType) {
         case 'insert':
-          this.rtTodos.on('insert', function(event) {
+          this.rtTodos.on('insert', function (event) {
             callback(event.value.value(), event.index);
           });
           break;
         case 'remove':
-          this.rtTodos.on('remove', function(event) {
+          this.rtTodos.on('remove', function (event) {
             callback(event.index);
           });
           break;
         case 'reorder':
-          this.rtTodos.on('reorder', function(event) {
+          this.rtTodos.on('reorder', function (event) {
             callback(event.fromIndex, event.toIndex);
           });
           break;
         case 'toggle':
           // Listen to any changes on the model, ignoring any that don't involve the 'completed' key.
           // We can get the appropriate todo from the relativePath of the event
-          this.rtTodos.on('model_changed', function(event) {
+          this.rtTodos.on('model_changed', function (event) {
             if (event.relativePath.length) {
               if (event.childEvent.name === 'set' && event.childEvent.key === 'completed') {
                 var changedTodo = event.element.elementAt(event.relativePath).value();
                 callback(changedTodo.id, changedTodo.completed);
-              } else if(event.childEvent.name.toLowerCase() === 'value' && event.relativePath[1] === 'completed') {
+              } else if (event.childEvent.name.toLowerCase() === 'value' && event.relativePath[1] === 'completed') {
                 var changedTodo = event.element.elementAt(event.relativePath[0]).value();
                 callback(changedTodo.id, changedTodo.completed);
               }
@@ -150,13 +152,13 @@
           break;
       }
     },
-    onTodoRemote: function(todoIndex, eventType, callback) {
+    onTodoRemote: function (todoIndex, eventType, callback) {
       var rtTodo = this.rtTodos.get(todoIndex);
       if (rtTodo) {
         var rtTitle = rtTodo.get('title');
-        switch(eventType) {
+        switch (eventType) {
           case 'text-change':
-            rtTitle.on('value', function(event) {
+            rtTitle.on('value', function (event) {
               callback(rtTodo.value().id, rtTodo.value().title);
             });
             break;
@@ -164,12 +166,12 @@
           // broadcasting these events yet from the App, so they won't get fired.  Currently, we just set the value
           // of the todo in edit mode when focus is lost or the 'enter' key is pressed.
           case 'text-remove':
-            rtTitle.on('remove', function(event) {
+            rtTitle.on('remove', function (event) {
               callback(rtTodo.value().id, event.index);
             });
             break;
           case 'text-insert':
-            rtTitle.on('insert', function(event) {
+            rtTitle.on('insert', function (event) {
               callback(rtTodo.value().id, event.index, event.value);
             });
             break;
