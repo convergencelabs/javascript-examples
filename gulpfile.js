5de72d4c92671feaaa0c1f99bac21533dfca5214
Michael MacFadden <michael@convergencelabs.com>
2018-12-25 09:45:56 -0700

    Updated to gulp 4.0.0, updated the node version, and fixed the publish repo for docker hub.

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,25 @@ const gulp = require('gulp');
 const del = require('del');
 const merge = require('merge2');
 
-gulp.task('clean', function() {
+const clean = () => {
   return del['docker-build'];
-});
+}
 
-gulp.task('default', ['docker-prep'], function() {
 
-});
-
-gulp.task('docker-prep', function () {
-  const libTasks = merge(libs.map(lib => gulp.src("node_modules/" + lib + "/**/*").pipe(gulp.dest('docker-build/src/libs/' + lib))));
+const dockerPrep = () => {
+  const libTasks = merge(
+    libs.map(lib => gulp.src("node_modules/" + lib + "/**/*")
+      .pipe(gulp.dest('docker-build/src/libs/' + lib))
+    )
+  );
 
   return merge(
     [merge([
-    gulp.src(['src/**/*']).pipe(gulp.dest('docker-build/src')),
-    gulp.src(['docker/**/*']).pipe(gulp.dest('docker-build'))
-  ]),
-      libTasks]);
-});
+      gulp.src(['src/**/*']).pipe(gulp.dest('docker-build/src')),
+      gulp.src(['docker/**/*']).pipe(gulp.dest('docker-build'))
+    ]), libTasks]
+  );
+}
 
 const libs = [
   "@convergence",
@@ -34,10 +35,13 @@ const libs = [
   "nouislider",
   "todomvc-common",
   "todomvc-app-css",
-  "jointjs-utils",
   "jointjs",
   "lodash",
   "backbone",
-  "underscore",
-  "dom-utils",
+  "underscore"
 ];
+
+module.exports = {
+  clean,
+  dockerPrep
+};
