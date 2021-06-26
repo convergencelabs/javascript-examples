a953e0944d41a403638160014f71eacb8bf1b67e
Michael MacFadden <michael@convergencelabs.com>
2021-06-26 11:31:16 -0500

    Tweaking build scripts.

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -2,7 +2,6 @@
 
 docker run --rm \
   --volume="$PWD:/srv/jekyll" \
-  --volume="$PWD/builder/bundle:/usr/local/bundle" \
   -p 4000:4000 \
   -it jekyll/jekyll:4.2.0 \
   jekyll serve --incremental --profile
